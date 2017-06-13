92f8ad5d7fefed19652d4b91b5e513ea0b14d42a
hthg <f.canas.s@gmail.com>
2017-06-13 13:50:15 -0700

    Project description updates

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject zaub "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Zaub, the game."
+  :url "https://github.com/hthg/zaub"
+  :license {:name "GNU General Public License v3"
+            :url "http://fsf.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.namespace "0.2.11"]
                  ]
